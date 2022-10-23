acfc4633138fdf7713c73cffe96bf4ff7c128533
Zony Yu <zony249@gmail.com>
2022-10-22 18:10:53 -0600

    Implemented Recipe Constructor

--- app/src/test/java/com/cmput301f22t09/shell379/RecipeLogicTest.java
+++ app/src/test/java/com/cmput301f22t09/shell379/RecipeLogicTest.java
@@ -1,7 +1,14 @@
 package com.cmput301f22t09.shell379;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import android.graphics.PixelFormat;
+import android.media.Image;
+import android.media.ImageReader;
+import android.util.Log;
 
 import com.cmput301f22t09.shell379.data.Recipe;
 
@@ -40,21 +47,44 @@ public class RecipeLogicTest {
     @Test
     public void testConstructor_002() {
         assertThrows(IllegalArgumentException.class, () -> {
-            Recipe rec = new Recipe(template_title, 0L, template_servings, template_cat, template_com);
-        }, "Preparation time must be positive definite.");
+            Recipe rec = new Recipe(template_title, -1L, template_servings, template_cat, template_com);
+        }, "Preparation time cannot be negative.");
     }
     @Test
     public void testConstructor_003() {
-
+        assertThrows(IllegalArgumentException.class, () -> {
+            Recipe rec = new Recipe(template_title, 0L, template_servings, template_cat, template_com);
+        }, "Preparation time must be greater than 0.");
     }
     @Test
     public void testConstructor_004() {
-
+        assertThrows(IllegalArgumentException.class, () -> {
+            Recipe rec = new Recipe(template_title, template_prep, 0, template_cat, template_com);
+        }, "Servings must be greater than 0");
     }
     @Test
     public void testConstructor_005() {
-
+        assertThrows(IllegalArgumentException.class, () -> {
+            Recipe rec = new Recipe(template_title, template_prep, -1, template_cat, template_com);
+        }, "Servings must be greater than 0");
     }
 
+
+    @Test
+    public void testConstructor_006() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Recipe rec = new Recipe(template_title, template_prep, template_servings, "", template_com);
+        }, "Category must be defined and non-empty");
+    }
+
+    @Test
+    public void testConstructor_007() {
+        try {
+            assertEquals(1, 0);
+            Recipe rec = new Recipe(template_title, template_prep, template_servings, template_cat, template_com, null);
+        } catch (Exception e) {
+            fail("This should not throw an exception");
+        }
+    }
 
 }
