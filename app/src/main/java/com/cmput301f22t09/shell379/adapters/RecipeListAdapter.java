7eb5455f8b379e3186fab3a21e96f64b621ecbcc
AlphonsoD <alphonso@ualberta.ca>
2022-10-30 13:18:18 -0600

    working on recipe list stuff

--- app/src/main/java/com/cmput301f22t09/shell379/adapters/RecipeListAdapter.java
+++ app/src/main/java/com/cmput301f22t09/shell379/adapters/RecipeListAdapter.java
@@ -6,10 +6,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cmput301f22t09.shell379.R;
 import com.cmput301f22t09.shell379.data.Recipe;
+import com.cmput301f22t09.shell379.fragments.RecipeListFragment;
 
 import java.util.ArrayList;
 
@@ -22,25 +27,38 @@ import java.util.ArrayList;
 public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder>{
 
     private ArrayList<Recipe> recipes;
+    private RecipeListFragment recipeListFragment;
 
-    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+    public class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView recipeName;
         TextView prepTime;
         TextView servings;
         TextView category;
+        private NavController navController;
 
-        public RecipeViewHolder(@NonNull View itemView) {
+        public RecipeViewHolder(@NonNull View itemView, Fragment fragment) {
             super(itemView);
-            this.recipeName = (TextView) itemView.findViewById(R.id.rli_name);
-            this.prepTime = (TextView) itemView.findViewById(R.id.rli_prep_time_textView);
-            this.servings = (TextView) itemView.findViewById(R.id.rli_servings_textView);
-            this.category = (TextView) itemView.findViewById(R.id.rli_category_textView);
+            this.recipeName = itemView.findViewById(R.id.rli_name);
+            this.prepTime = itemView.findViewById(R.id.rli_prep_time_textView);
+            this.servings = itemView.findViewById(R.id.rli_servings_textView);
+            this.category = itemView.findViewById(R.id.rli_category_textView);
+            // TODO: cite https://developer.android.com/guide/navigation/navigation-getting-started
+            // for how to get nav controller
+            this.navController = NavHostFragment.findNavController(fragment);
+        }
+
+        // TODO: cite this later https://stackoverflow.com/questions/24885223/why-doesnt-recyclerview-have-onitemclicklistener
+        // used this to reference how to create an on click listener for recycler view items
+        @Override
+        public void onClick(View view) {
+
         }
     }
 
-    public RecipeListAdapter(ArrayList<Recipe> data) {
+    public RecipeListAdapter(ArrayList<Recipe> data, RecipeListFragment recipeListFragment) {
         this.recipes = data;
+        this.recipeListFragment = recipeListFragment;
     }
 
     @NonNull
@@ -48,7 +66,7 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.Re
     public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.recipe_list_item, parent, false);
-        RecipeViewHolder recipeViewHolder = new RecipeViewHolder(view);
+        RecipeViewHolder recipeViewHolder = new RecipeViewHolder(view, recipeListFragment);
         return recipeViewHolder;
     }
 
@@ -69,4 +87,5 @@ public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.Re
     public int getItemCount() {
         return recipes.size();
     }
+
 }
