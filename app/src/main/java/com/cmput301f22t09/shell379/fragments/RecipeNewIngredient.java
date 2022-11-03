fe8ff793cd5ee27b9886b8f90fbec8687acf253a
elsa chen <elsachen0907@gmail.com>
2022-11-03 16:16:19 -0600

    implment the recipe-new-ingredient

--- app/src/main/java/com/cmput301f22t09/shell379/fragments/RecipeNewIngredient.java
+++ app/src/main/java/com/cmput301f22t09/shell379/fragments/RecipeNewIngredient.java
@@ -3,10 +3,12 @@ package com.cmput301f22t09.shell379.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.cmput301f22t09.shell379.R;
 
@@ -16,6 +18,7 @@ import com.cmput301f22t09.shell379.R;
  * create an instance of this fragment.
  */
 public class RecipeNewIngredient extends Fragment {
+    private NavController navController;
 
     public RecipeNewIngredient() {
         // Required empty public constructor
@@ -38,7 +41,26 @@ public class RecipeNewIngredient extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.recipe_new_ingredient, container, false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.recipe_new_ingredient, container, false);
+//        return inflater.inflate(R.layout.recipe_new_ingredient, container, false);
+
+        ((ImageView)rootView.findViewById(R.id.back)).setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        back();
+                    }
+                }
+        );
+        return rootView;
+    }
+
+
+    /**
+     * Implement the option to go back to previous page
+     */
+    private void back(){
+        navController.popBackStack();
     }
 }
