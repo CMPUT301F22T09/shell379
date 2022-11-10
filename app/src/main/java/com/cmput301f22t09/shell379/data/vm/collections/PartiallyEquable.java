966c182b0c9b592e5049bcb2c731650ee869cefe
josh <jyw2@ualberta.ca>
2022-11-09 21:38:45 -0700

    No bugs

--- app/src/main/java/com/cmput301f22t09/shell379/data/vm/collections/PartiallyEquable.java
+++
@@ -1,9 +0,0 @@
-package com.cmput301f22t09.shell379.data.vm.collections;
-/**
- * Interface for classes that support partial equality
- */
-public interface PartiallyEquable {
-    public boolean partialEquals(PartiallyEquable other);
-    public boolean isFull();
-    public void combine(PartiallyEquable e);
-}
