dd13d8fda8641459697fea187862543cd301a291
josh <jyw2@ualberta.ca>
2022-11-11 16:30:19 -0700

    Comments

--- app/src/main/java/com/cmput301f22t09/shell379/data/util/ArraySortUtil.java
+++ app/src/main/java/com/cmput301f22t09/shell379/data/util/ArraySortUtil.java
@@ -3,204 +3,116 @@ package com.cmput301f22t09.shell379.data.util;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 
-
+/**
+ *  sorting utility functions
+ */
 public class ArraySortUtil {
 
     public interface StringPropGetter{
         public String getString(Object object);
     }
 
-//    /**
-//     * sorts an array list by the specified string property on the object using the property's compareTo
-//     * method. Uses a quicksort internally.
-//     * The arraylist 'objects' list will be mutated and returned.
-//     * @param objects list of objects to sort. Will be mutated!
-//     * @param propertyName property to sort on
-//     */
-//    public static <T> ArrayList<T> sortByStringProp(ArrayList<T> objects, String propertyName, StringPropGetter stringPropGetter){
-//        // https://docs.oracle.com/javase/8/docs/api/java/lang/reflect/Field.html
-//        // Dynamic prop referencing from https://stackoverflow.com/questions/13128194/java-how-can-i-dynamically-reference-an-objects-property
-//        // Generics documentation https://docs.oracle.com/javase/tutorial/extra/generics/methods.html
-//
-//        // check type and if property exists
-//        if(objects.size() == 0){
-//            return objects;
-//        }
-//        Field field;
-//        try{
-//
-//            field = objects.get(0).getClass().getDeclaredField(propertyName);
-//        }catch (Exception e){
-//            throw new IllegalArgumentException("object does not contain property: " +propertyName);
-//        }
-//        try{
-//            if(!(field.get(objects.get(0)) instanceof String)){
-//                throw new IllegalArgumentException("property to sort on is not of type string");
-//            }
-//        }catch (Exception e){
-//            throw new IllegalArgumentException("object does not contain property: " +propertyName);
-//        }
-//
-//        quickSortOnString(objects,propertyName,0,objects.size() - 1);
-//
-//        return objects;
-//    }
-//
-//    private static <T> void quickSortOnString(ArrayList<T> objects, String propertyName, int low, int high) {
-//        // Quick sort algorithm from https://www.geeksforgeeks.org/quick-sort/
-//        if (low < high) {
-//
-//            /* pi is partitioning index, arr[pi] is now at right place */
-//            int pi = partitionOnString(objects, propertyName, low, high);
-//
-//            quickSortOnString(objects,propertyName, low, pi-1);  // Before pi
-//
-//            quickSortOnString(objects, propertyName,pi + 1, high); // After pi
-//
-//        }
-//    }
-//
-//    private static <T> int partitionOnString (ArrayList<T> objects, String propertyName, int low, int high)
-//    {
-//        // Quick sort algorithm from https://www.geeksforgeeks.org/quick-sort/
-//        // pivot (Element to be placed at right position)
-//        T pivot = objects.get(high);
-//
-//        int i = (low - 1);  // Index of smaller element and indicates the
-//        // right position of pivot found so far
-//
-//        for (int j = low; j <= high - 1; j++){
-//
-//            // If current element is smaller than the pivot
-//            if (isSmallerString(objects.get(j), objects.get(i),propertyName)){
-//                i++;    // increment index of smaller element
-//                T tempI = objects.get(i);
-//                objects.set(i,objects.get(j));
-//                objects.set(j,tempI);
-//            }
-//        }
-//        T tempI = objects.get(i + 1);
-//        objects.set(i,objects.get(high));
-//        objects.set(high,tempI);
-//
-//        return (i + 1);
-//    }
-//
-//    private static <T> boolean isSmallerString(T left,T right,String propertyName){
-//        Field field;
-//        String leftValue;
-//        String rightValue;
-//        try{
-//            // https://stackoverflow.com/questions/13128194/java-how-can-i-dynamically-reference-an-objects-property
-//            field = left.getClass().getDeclaredField(propertyName);
-//            leftValue = (String)field.get(left);
-//            rightValue = (String)field.get(right);
-//        }catch (Exception e){
-//            throw new IllegalArgumentException("T does not contain property: " +propertyName);
-//        }
-//
-//        if (left == null && right == null ||
-//                left != null && right == null){
-//            return false;
-//        }else if(left == null && right != null ||
-//                leftValue.compareTo(rightValue) < 0){
-//            return true;
-//        }else{
-//            return  false;
-//        }
-//    }
+    /**
+     * sorts an array list by the specified string property on the object using the string
+     * property's compareTo method. Uses a quicksort internally.
+     * The arraylist 'objects' list will be mutated and returned.
+     * @param objects list of objects to sort. Will be mutated!
+     * @param stringPropGetter Class that contains the getString method to get the string representation of
+     *              the property to sort on.
+     */
+    public static <T> ArrayList<T> sortByStringProp(ArrayList<T> objects, StringPropGetter stringPropGetter){
+        // https://docs.oracle.com/javase/8/docs/api/java/lang/reflect/Field.html
+        // Dynamic prop referencing from https://stackoverflow.com/questions/13128194/java-how-can-i-dynamically-reference-an-objects-property
+        // Generics documentation https://docs.oracle.com/javase/tutorial/extra/generics/methods.html
 
-
-        /**
-         * sorts an array list by the specified string property on the object using the property's compareTo
-         * method. Uses a quicksort internally.
-         * The arraylist 'objects' list will be mutated and returned.
-         * @param objects list of objects to sort. Will be mutated!
-//         * @param propertyName property to sort on
-         */
-        public static <T> ArrayList<T> sortByStringProp(ArrayList<T> objects, StringPropGetter stringPropGetter){
-            // https://docs.oracle.com/javase/8/docs/api/java/lang/reflect/Field.html
-            // Dynamic prop referencing from https://stackoverflow.com/questions/13128194/java-how-can-i-dynamically-reference-an-objects-property
-            // Generics documentation https://docs.oracle.com/javase/tutorial/extra/generics/methods.html
-
-            // check type and if property exists
-            if(objects.size() == 0){
-                return objects;
-            }
-//            Field field;
-//            try{
-//
-//                field = objects.get(0).getClass().getDeclaredField(propertyName);
-//            }catch (Exception e){
-//                throw new IllegalArgumentException("object does not contain property: " +propertyName);
-//            }
-//            try{
-//                if(!(field.get(objects.get(0)) instanceof String)){
-//                    throw new IllegalArgumentException("property to sort on is not of type string");
-//                }
-//            }catch (Exception e){
-//                throw new IllegalArgumentException("object does not contain property: " +propertyName);
-//            }
-
-            quickSortOnString(objects,stringPropGetter,0,objects.size() - 1);
-
+        // check type and if property exists
+        if(objects.size() == 0){
             return objects;
         }
 
-        private static <T> void quickSortOnString(ArrayList<T> objects, StringPropGetter stringPropGetter, int low, int high) {
-            // Quick sort algorithm from https://www.geeksforgeeks.org/quick-sort/
-            if (low < high) {
+        quickSortOnString(objects,stringPropGetter,0,objects.size() - 1);
 
-                /* pi is partitioning index, arr[pi] is now at right place */
-                int pi = partitionOnString(objects, stringPropGetter, low, high);
+        return objects;
+    }
 
-                quickSortOnString(objects,stringPropGetter, low, pi-1);  // Before pi
+    /**
+     * recursive quick sort on an object's string property.
+     * @param objects objects to sort
+     * @param stringPropGetter implements the way to get the string prop from the object
+     * @param low index to start sorting
+     * @param high index to end sorting
+     * @param <T> type of objects to sort
+     */
+    private static <T> void quickSortOnString(ArrayList<T> objects, StringPropGetter stringPropGetter, int low, int high) {
+        // Quick sort algorithm from https://www.geeksforgeeks.org/quick-sort/
+        if (low < high) {
 
-                quickSortOnString(objects, stringPropGetter,pi + 1, high); // After pi
+            /* pi is partitioning index, arr[pi] is now at right place */
+            int pi = partitionOnString(objects, stringPropGetter, low, high);
 
+            quickSortOnString(objects,stringPropGetter, low, pi-1);  // Before pi
+
+            quickSortOnString(objects, stringPropGetter,pi + 1, high); // After pi
+
+        }
+    }
+
+    /**
+     *  part of the quicksort algorithm. partitions the array
+     * @param objects objects to partition
+     * @param stringPropGetter implements the way to get the string prop from the object
+     * @param low index to start partition
+     * @param high index to end partition
+     * @param <T> type of objects to sort
+     * @return partition index
+     */
+    private static <T> int partitionOnString (ArrayList<T> objects, StringPropGetter stringPropGetter, int low, int high)
+    {
+        // Quick sort algorithm from https://www.geeksforgeeks.org/quick-sort/
+        // pivot (Element to be placed at right position)
+        T pivot = objects.get(high);
+
+        int i = (low - 1);  // Index of smaller element and indicates the
+        // right position of pivot found so far
+
+        for (int j = low; j <= high - 1; j++){
+
+            // If current element is smaller than the pivot
+            if (isSmallerString(objects.get(j), pivot, stringPropGetter)){
+                i++;    // increment index of smaller element
+                T tempI = objects.get(i);
+                objects.set(i,objects.get(j));
+                objects.set(j,tempI);
             }
         }
+        T tempI = objects.get(i + 1);
+        objects.set(i + 1,objects.get(high));
+        objects.set(high,tempI);
 
-        private static <T> int partitionOnString (ArrayList<T> objects, StringPropGetter stringPropGetter, int low, int high)
-        {
-            // Quick sort algorithm from https://www.geeksforgeeks.org/quick-sort/
-            // pivot (Element to be placed at right position)
-            T pivot = objects.get(high);
+        return (i + 1);
+    }
 
-            int i = (low - 1);  // Index of smaller element and indicates the
-            // right position of pivot found so far
+    /**
+     * part of the quicksort algorithm. compares the string fields of two objects.Nulls are ok.
+     * @param left left object with a string param
+     * @param right right object with a string param
+     * @param stringPropGetter defines how to get the string param from the object
+     * @param <T> type of object
+     * @return true if left is smaller than right, false otherwise.
+     * A null param is always considered smaller than a non null param.
+     */
+    private static <T> boolean isSmallerString(T left,T right,StringPropGetter stringPropGetter){
+        String leftValue = stringPropGetter.getString(left);
+        String rightValue = stringPropGetter.getString(right);
 
-            for (int j = low; j <= high - 1; j++){
-
-                // If current element is smaller than the pivot
-                if (isSmallerString(objects.get(j), pivot, stringPropGetter)){
-                    i++;    // increment index of smaller element
-                    T tempI = objects.get(i);
-                    objects.set(i,objects.get(j));
-                    objects.set(j,tempI);
-                }
-            }
-            T tempI = objects.get(i + 1);
-            objects.set(i + 1,objects.get(high));
-            objects.set(high,tempI);
-
-            return (i + 1);
+        if (left == null && right == null ||
+                left != null && right == null){
+            return false;
+        }else if(left == null && right != null ||
+                leftValue.compareTo(rightValue) < 0){
+            return true;
+        }else{
+            return  false;
         }
-
-        private static <T> boolean isSmallerString(T left,T right,StringPropGetter stringPropGetter){
-            String leftValue = stringPropGetter.getString(left);
-            String rightValue = stringPropGetter.getString(right);
-
-            if (left == null && right == null ||
-                    left != null && right == null){
-                return false;
-            }else if(left == null && right != null ||
-                    leftValue.compareTo(rightValue) < 0){
-                return true;
-            }else{
-                return  false;
-            }
-        }
-
-
+    }
 }
