df87686483e42017fd8498be11a88af26ce119d8
josh <jyw2@ualberta.ca>
2022-11-11 13:21:39 -0700

    Fix bug

--- app/src/main/java/com/cmput301f22t09/shell379/data/util/ArraySortUtil.java
+++ app/src/main/java/com/cmput301f22t09/shell379/data/util/ArraySortUtil.java
@@ -11,57 +11,57 @@ import java.util.ArrayList;
  * @param propertyName property to sort on
  */
 public class ArraySortUtil {
-    public static ArrayList<Object> sortBy(ArrayList<Object> objects, String propertyName){
-        if(objects.size() == 0){
-            throw new IllegalArgumentException("Array list cannot be empty for sortBy");
-        }
-        try{
-            // https://stackoverflow.com/questions/13128194/java-how-can-i-dynamically-reference-an-objects-property
-            Field field;
-            field = objects.get(0).getClass().getDeclaredField(propertyName);
-        }catch (Exception e){
-            throw new IllegalArgumentException("Object does not contain property: " +propertyName);
-        }
-        quickSort(objects,propertyName,0,objects.size() - 1);
-
-        return objects;
-    }
-
-    private static ArrayList<Object> quickSort(ArrayList<Object> objects, String propertyName, int low, int high) {
-        // Quick sort algorithm from https://www.geeksforgeeks.org/quick-sort/
-        if (low < high) {
-
-            /* pi is partitioning index, arr[pi] is now at right place */
-            int pi = partition(objects, propertyName, low, high);
-
-            quickSort(objects,propertyName, low, pi-1);  // Before pi
-
-            quickSort(objects, propertyName,pi + 1, high); // After pi
-
-        }
-
-    }
-
-    private static int partition (ArrayList<Object> objects, String propertyName, int low, int high)
-    {
-        // Quick sort algorithm from https://www.geeksforgeeks.org/quick-sort/
-        // pivot (Element to be placed at right position)
-        Object pivot = objects.get(high);
-
-        int i = (low - 1);  // Index of smaller element and indicates the
-        // right position of pivot found so far
-
-        for (int j = low; j <= high - 1; j++){
-
-            // If current element is smaller than the pivot
-            if (objects.get(j) < pivot){
-                i++;    // increment index of smaller element
-                swap arr[i] and arr[j]
-            }
-        }
-        swap arr[i + 1] and arr[high])
-        return (i + 1)
-    }
+//    public static ArrayList<Object> sortBy(ArrayList<Object> objects, String propertyName){
+//        if(objects.size() == 0){
+//            throw new IllegalArgumentException("Array list cannot be empty for sortBy");
+//        }
+//        try{
+//            // https://stackoverflow.com/questions/13128194/java-how-can-i-dynamically-reference-an-objects-property
+//            Field field;
+//            field = objects.get(0).getClass().getDeclaredField(propertyName);
+//        }catch (Exception e){
+//            throw new IllegalArgumentException("Object does not contain property: " +propertyName);
+//        }
+//        quickSort(objects,propertyName,0,objects.size() - 1);
+//
+//        return objects;
+//    }
+//
+//    private static ArrayList<Object> quickSort(ArrayList<Object> objects, String propertyName, int low, int high) {
+//        // Quick sort algorithm from https://www.geeksforgeeks.org/quick-sort/
+//        if (low < high) {
+//
+//            /* pi is partitioning index, arr[pi] is now at right place */
+//            int pi = partition(objects, propertyName, low, high);
+//
+//            quickSort(objects,propertyName, low, pi-1);  // Before pi
+//
+//            quickSort(objects, propertyName,pi + 1, high); // After pi
+//
+//        }
+//
+//    }
+//
+//    private static int partition (ArrayList<Object> objects, String propertyName, int low, int high)
+//    {
+//        // Quick sort algorithm from https://www.geeksforgeeks.org/quick-sort/
+//        // pivot (Element to be placed at right position)
+//        Object pivot = objects.get(high);
+//
+//        int i = (low - 1);  // Index of smaller element and indicates the
+//        // right position of pivot found so far
+//
+//        for (int j = low; j <= high - 1; j++){
+//
+//            // If current element is smaller than the pivot
+//            if (objects.get(j) < pivot){
+//                i++;    // increment index of smaller element
+//                swap arr[i] and arr[j]
+//            }
+//        }
+//        swap arr[i + 1] and arr[high])
+//        return (i + 1)
+//    }
 
 
 }
