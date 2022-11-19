8e14b761b80b205f26e770d269364110dd1211a1
elsa chen <elsachen0907@gmail.com>
2022-11-18 20:51:43 -0700

    use of mp viewmodel

--- app/src/main/java/com/cmput301f22t09/shell379/fragments/AddRecipeToMPFragment.java
+++ app/src/main/java/com/cmput301f22t09/shell379/fragments/AddRecipeToMPFragment.java
@@ -8,43 +8,54 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cmput301f22t09.shell379.R;
+import com.cmput301f22t09.shell379.adapters.AddRecipeMealPlanAdapter;
 import com.cmput301f22t09.shell379.data.Ingredient;
+import com.cmput301f22t09.shell379.data.Recipe;
 import com.cmput301f22t09.shell379.data.vm.Environment;
+import com.cmput301f22t09.shell379.data.vm.MealPlanViewModel;
+import com.cmput301f22t09.shell379.data.wrapper.MealPlanWrapper;
 
-public class AddRecipeToMPFragment extends Fragment {
-    private Environment envViewModel;
+import java.util.ArrayList;
+
+public class AddRecipeToMPFragment extends DialogFragment implements AddRecipeMealPlanAdapter.RecipeInMealPlanListener {
+    private Environment env;
     private NavController navController;
-    RecyclerView addRecipeToMP_recyclerView;
-    RecyclerView.LayoutManager layoutManager;
-    int selectedSortIndex;
+    private MealPlanViewModel mealPlanViewModel;
+    AddRecipeMealPlanAdapter addRecipeMealPlanAdapter;
+    ArrayList<MealPlanWrapper<Recipe>> RecipeinMealPlan;
+ //    RecyclerView addRecipeToMP_recyclerView;
+//    RecyclerView.LayoutManager layoutManager;
+//    int selectedSortIndex;
+
 
     public AddRecipeToMPFragment(){
 
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mealPlanViewModel = new ViewModelProvider(requireActivity()).get(MealPlanViewModel.class);
+
+        navController = NavHostFragment.findNavController(this);
+        env = Environment.of((AppCompatActivity) requireActivity());
+    }
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView =  inflater.inflate(R.layout.fragment_add_recipe_meal_plan_16, container, false);
-
-//        To-do
-        // Implement the spinner option to sort the ingredient list
-//        Spinner spinner = (Spinner) rootView.findViewById(R.id.add_recipe_toMP_sort_spinner);
-//        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(
-//                getActivity(),
-//                android.R.layout.simple_spinner_item,
-////                MealPlan.getSortableProps()
-//        );
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter);
-//        spinner.setSelection(0);
-//        selectedSortIndex = 0;
 
         // Implement the button to back to previous page
         ((ImageView)rootView.findViewById(R.id.back_button)).setOnClickListener(
@@ -55,10 +66,19 @@ public class AddRecipeToMPFragment extends Fragment {
                 }
         );
 
+        //        To-do sorting
+
         return rootView;
     }
 
     private void back(){
         navController.popBackStack();
     }
+
+    /**
+     *  Moves to the edit recipe in meal plan fragment
+     */
+    public void editRecipeInMP(int index){
+//        navController.navigate(AddRecipeToMPFragment);
+    };
 }
