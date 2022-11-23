5d7bae45007aa187836c36c57af1ed51e2cde8b9
1yimeng <yimwang6@gmail.com>
2022-11-22 20:30:59 -0700

    auto filling edited ingredient details

--- app/src/main/java/com/cmput301f22t09/shell379/fragments/CheckoutIngredientFragment.java
+++ app/src/main/java/com/cmput301f22t09/shell379/fragments/CheckoutIngredientFragment.java
@@ -23,8 +23,10 @@ import com.cmput301f22t09.shell379.data.Ingredient;
 import com.cmput301f22t09.shell379.data.vm.Environment;
 import com.cmput301f22t09.shell379.data.wrapper.CartIngredient;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class CheckoutIngredientFragment extends Fragment {
     protected View rootView;
@@ -38,6 +40,7 @@ public class CheckoutIngredientFragment extends Fragment {
     private TextView amountRequired;
     private EditText amountPurchased;
     private TextView unit;
+    private DatePicker dateSelect;
 
     public CheckoutIngredientFragment() {
         // Required empty public constructor
@@ -62,12 +65,24 @@ public class CheckoutIngredientFragment extends Fragment {
         amountRequired = ((TextView) rootView.findViewById(R.id.amountRequired));
         unit = ((TextView) rootView.findViewById(R.id.editUnit));
         amountPurchased = ((EditText) rootView.findViewById(R.id.editAmount));
+        dateSelect = ((DatePicker) rootView.findViewById(R.id.editBestBeforeDate));
 
         theCartIngredient = envViewModel.getCart().getList().get(ingredientIndex);
         description.setText(theCartIngredient.getDescription());
         category.setText(theCartIngredient.getCategory());
         amountRequired.setText(theCartIngredient.getAmount().toString());
         unit.setText(theCartIngredient.getUnit());
+
+        if (theCartIngredient.getIngredient() != null) {
+            location.setText(theCartIngredient.getIngredient().getLocation());
+            amountPurchased.setText(theCartIngredient.getIngredient().getAmount().toString());
+            Calendar cal = Calendar.getInstance(TimeZone.getDefault());
+            cal.setTime(theCartIngredient.getIngredient().getBestBefore());
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+            int day = cal.get(Calendar.DAY_OF_MONTH);
+            dateSelect.updateDate(year, month, day);
+        }
 
         ((ImageView) rootView.findViewById(R.id.back)).setOnClickListener(
                 new View.OnClickListener() {
@@ -108,38 +123,37 @@ public class CheckoutIngredientFragment extends Fragment {
         try {
 
             // Load data from Views
-            String description = ((TextView) rootView.findViewById(R.id.editDescription)).getText().toString();
-            String location = ((EditText) rootView.findViewById(R.id.editLocation)).getText().toString();
-            String strAmount = ((EditText) rootView.findViewById(R.id.editAmount)).getText().toString();
-            String category = ((TextView) rootView.findViewById(R.id.editCategory)).getText().toString();
-            String unit = ((TextView) rootView.findViewById(R.id.editUnit)).toString();
+            String descriptionIngredient = description.getText().toString();
+            String locationIngredient = location.getText().toString();
+            String strAmountIngredient = amountPurchased.getText().toString();
+            String categoryIngredient = category.getText().toString();
+            String unitIngredient = unit.toString();
 
             // validate
-            if (description.isEmpty() ||
-                    location.isEmpty() ||
-                    category.isEmpty() ||
-                    unit.isEmpty() ||
-                    strAmount.isEmpty()
+            if (descriptionIngredient.isEmpty() ||
+                    locationIngredient.isEmpty() ||
+                    categoryIngredient.isEmpty() ||
+                    unitIngredient.isEmpty() ||
+                    strAmountIngredient.isEmpty()
             ) {
                 showError("All fields need to be filled");
                 return;
             }
 
             int amount = 0;
-            if (strAmount.isEmpty()) {
+            if (strAmountIngredient.isEmpty()) {
                 showError("Amount not filled");
                 return;
             } else {
-                amount = Integer.parseInt(strAmount);
+                amount = Integer.parseInt(strAmountIngredient);
             }
 
-            DatePicker bestBeforeDatePicker = rootView.findViewById(R.id.editBestBeforeDate);
             Date bestBeforeDate = new GregorianCalendar(
-                    bestBeforeDatePicker.getYear(),
-                    bestBeforeDatePicker.getMonth(),
-                    bestBeforeDatePicker.getDayOfMonth()).getTime();
+                    dateSelect.getYear(),
+                    dateSelect.getMonth(),
+                    dateSelect.getDayOfMonth()).getTime();
 
-            Ingredient newIng = new Ingredient(description, bestBeforeDate, location, amount, unit, category);
+            Ingredient newIng = new Ingredient(descriptionIngredient, bestBeforeDate, locationIngredient, amount, unitIngredient, categoryIngredient);
 
             writeToViewModel(newIng);
             navController.popBackStack();
