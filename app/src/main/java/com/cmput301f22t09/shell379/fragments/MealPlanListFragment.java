e6ad91dc21f73be393712a8daa3bc9294b8b0bf6
josh <jyw2@ualberta.ca>
2022-11-23 16:56:33 -0700

    quick fix

--- app/src/main/java/com/cmput301f22t09/shell379/fragments/MealPlanListFragment.java
+++ app/src/main/java/com/cmput301f22t09/shell379/fragments/MealPlanListFragment.java
@@ -87,7 +87,7 @@ public class MealPlanListFragment extends Fragment implements MealPlanAdapter.Ad
                 new View.OnClickListener() {
                     public void onClick(View v) {
                         MealPlanViewModel.of(new MealPlan(), requireActivity());
-                        navController.navigate(MealPlanListFragmentDirections.actionMealPlanFragmentToCreateMealPlanFragment());
+                        navController.navigate(MealPlanListFragmentDirections.actionMealPlanFragmentToEditMealPlanFragment());
                     }
                 }
         );
