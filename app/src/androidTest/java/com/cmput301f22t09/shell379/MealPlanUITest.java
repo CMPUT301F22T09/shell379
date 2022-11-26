258fe282933bf09b6e11d723840e20837f340380
AlphonsoD <alphonso@ualberta.ca>
2022-11-26 16:19:57 -0700

    Started meal plan ui testing)

---
+++ app/src/androidTest/java/com/cmput301f22t09/shell379/MealPlanUITest.java
@@ -0,0 +1,14 @@
+package com.cmput301f22t09.shell379;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+
+public class MealPlanUITest {
+
+    /* Starts in the main activity (home screen) */
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    
+}
