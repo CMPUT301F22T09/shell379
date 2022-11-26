607495928178965ce761365d9e5712a480d24480
elsa chen <elsachen0907@gmail.com>
2022-11-26 16:04:14 -0700

    fix merge conflict

--- app/src/main/java/com/cmput301f22t09/shell379/data/MealPlan.java
+++ app/src/main/java/com/cmput301f22t09/shell379/data/MealPlan.java
@@ -79,9 +79,9 @@ public class MealPlan implements Serializable {
     }
 
     public void setStartDate(Date startDate) {
-<<<<<<< HEAD
-        this.startDate = formatter.format(startDate);
-//        this.startDate=startDate;
+//<<<<<<< HEAD
+//        this.startDate = formatter.format(startDate);
+////        this.startDate=startDate;
 =======
         this.startDate = startDate;
 >>>>>>> dev
