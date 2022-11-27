8e788b6a8411e0a55ce7c4113fd2fe09908fde3b
AlphonsoD <alphonso@ualberta.ca>
2022-11-26 18:30:48 -0700

    almost finished meal plan ui intent testing first case

--- app/src/androidTest/java/com/cmput301f22t09/shell379/MealPlanUITest.java
+++ app/src/androidTest/java/com/cmput301f22t09/shell379/MealPlanUITest.java
@@ -2,17 +2,28 @@ package com.cmput301f22t09.shell379;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.util.Log;
 
 import androidx.test.espresso.PerformException;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import com.cmput301f22t09.shell379.data.Ingredient;
+import com.cmput301f22t09.shell379.data.IngredientStub;
+import com.cmput301f22t09.shell379.data.Recipe;
 import com.cmput301f22t09.shell379.data.vm.Environment;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class MealPlanUITest {
 
@@ -54,13 +65,58 @@ public class MealPlanUITest {
         }
 
         activityRule.getScenario().onActivity(activity -> {
-            // use 'activity'.
-            Environment.of(activity, new Environment());
+            Environment env = new Environment();
 
+            // Initialize env with one ingredient
+            Ingredient ingredient = new Ingredient("Chicken Thighs", "Freezer", 2, "kilograms", "Poultry");
+            env.getIngredients().add(ingredient);
+
+            // Initialize env with one recipe
+            IngredientStub stub = new IngredientStub("Milk", 2, "liters", "Dairy");
+            Recipe recipe = new Recipe("Milk recipe", 2L, 3, "Dairy", "comment");
+            recipe.addIngredient(stub);
+            env.getRecipes().add(recipe);
+
+            Environment.of(activity, env);
         });
 
-        
+        onView(withText("Meal Plan")).check(matches(isDisplayed()));
 
+        // Action 30
+        onView(withId(R.id.new_button)).perform(click());
+
+        // Set meal plan name and comments
+        onView(withId(R.id.plan_edit_name)).perform(replaceText("Chicken with milk"));
+        onView(withId(R.id.plan_edit_comment_txt)).perform(replaceText("This is a comment!"));
+
+        // Action 31
+        onView(withId(R.id.plan_edit_add_recipes_btn)).perform(scrollTo(), click());
+
+        // Action 32
+        final int[] pos = {0};
+        onView(withId(R.id.add_recipe_to_mealPlan_recyclerView)).perform(
+                actionOnItemAtPosition(pos[0], click()));
+
+        // Action 33
+        onView(withId(R.id.save_button)).perform(scrollTo(), click());
+
+        // Action 34
+        onView(withId(R.id.plan_edit_add_ingr_btn)).perform(scrollTo(), click());
+
+        // Action 35
+        onView(withId(R.id.add_ingredient_to_mealPlan_recyclerView)).perform(
+                actionOnItemAtPosition(pos[0], click()));
+
+        // TODO: set ingredient serving on date screen
+        onView(withId(R.id.serving_edittext)).perform(replaceText("2"));
+
+        // Action 36
+        onView(withId(R.id.save_button)).perform(scrollTo(), click());
+
+        // Action 39
+        onView(withId(R.id.mpe_save_plan)).perform(scrollTo(), click());
+
+        // TODO: check that text looks like what its supposed to in the recycler view
     }
 
 }
